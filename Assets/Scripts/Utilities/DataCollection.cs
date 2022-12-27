be7389268084e2a6258e18237448631d035ee1a5
Hengshuo <shenhengshuo@gmail.com>
2022-12-26 22:06:57 -0700

    11. initialize backpack data
    create item data structure

--- Assets/Scripts/Utilities/DataCollection.cs
+++ Assets/Scripts/Utilities/DataCollection.cs
@@ -3,17 +3,29 @@ using UnityEngine;
 [System.Serializable]
 public class ItemDetails
 {
+    [field: SerializeField]
     public int ItemID { get; set; }
-    public string name;
-    public ItemType itemType;
-    public Sprite itemIcon;
-    public Sprite itemOnWorldSprite;
-    public string itemDescription;
-    public int itemUseRadius;
-    public bool canPickedup;
-    public bool canDropped;
-    public bool canCarried;
-    public int itemPrice;
-    [Range(0f, 1f)]
-    public float sellPercentage;
+    [field: SerializeField]
+    public string Name { get; set; }
+    [field: SerializeField]
+    public ItemType ItemType { get; set; }
+    [field: SerializeField]
+    public Sprite ItemIcon { get; set; }
+    [field: SerializeField]
+    public Sprite ItemOnWorldSprite { get; set; }
+    [field: SerializeField]
+    public string ItemDescription { get; set; }
+    [field: SerializeField]
+    public int ItemUseRadius { get; set; }
+    [field: SerializeField]
+    public bool CanPickedup { get; set; }
+    [field: SerializeField]
+    public bool CanDropped { get; set; }
+    [field: SerializeField]
+    public bool CanCarried { get; set; }
+    [field: SerializeField]
+    public int ItemPrice { get; set; }
+    [field: Range(0f, 1f)]
+    [field: SerializeField]
+    public float SellPercentage { get; set; }
 }
