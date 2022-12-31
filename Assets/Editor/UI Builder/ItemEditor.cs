bd5da666324af8413c4f7af68ff0614a67eef99f
Hengshuo <shenhengshuo@gmail.com>
2022-12-30 23:12:21 -0700

    15.
    bind all fields in item editor with properties of item in database (ItemDataList_SO)

--- Assets/Editor/UI Builder/ItemEditor.cs
+++ Assets/Editor/UI Builder/ItemEditor.cs
@@ -4,13 +4,17 @@ using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 using System.Collections.Generic;
 using System;
-
+using System.Linq;
 
 public class ItemEditor : EditorWindow
 {
     private ItemDataList_SO Database { get; set; }
     private List<ItemDetails> ItemList { get; set; }
     private VisualTreeAsset ItemRowTemplate { get; set; }
+    private ScrollView ItemDetailsSection { get; set; }
+    private ItemDetails ActiveItem { get; set; }
+    private Sprite DefaultIcon { get; set; }
+    private VisualElement IconPreview { get; set; }
     private ListView ItemListView { get; set; }
 
     [MenuItem("M STUDIO/ItemEditor")]
@@ -43,8 +47,14 @@ public class ItemEditor : EditorWindow
 
         //retrive the template using absolute path
         ItemRowTemplate = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UI Builder/ItemRowTemplate.uxml");
+        //retrive default icon
+        DefaultIcon = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/M Studio/Art/Items/Icons/icon_M.png");
         // find ListView in UI
         ItemListView = root.Q<VisualElement>("ItemList").Q<ListView>("ListView");
+
+        ItemDetailsSection = root.Q<ScrollView>("ItemDetails");
+
+        IconPreview = ItemDetailsSection.Q<VisualElement>("Icon");
 
         LoadDatabase();
 
@@ -80,8 +90,103 @@ public class ItemEditor : EditorWindow
             };
         };
 
+        ItemListView.fixedItemHeight = 60;
         ItemListView.itemsSource = ItemList;
         ItemListView.makeItem = makeItem;
         ItemListView.bindItem = bindItem;
+
+        ItemListView.onSelectionChange += OnListSelectionChange;
+        //Detail information section is invisible before choose a item
+        ItemDetailsSection.visible = false;
+    }
+
+    private void OnListSelectionChange(IEnumerable<object> selectedItem)
+    {
+        ActiveItem = (ItemDetails)selectedItem.First();
+        GetItemDetails();
+        ItemDetailsSection.visible = true;
+    }
+
+    private void GetItemDetails()
+    {
+        ItemDetailsSection.MarkDirtyRepaint();
+
+        ItemDetailsSection.Q<IntegerField>("ItemID").value = ActiveItem.ItemID;
+        ItemDetailsSection.Q<IntegerField>("ItemID").RegisterValueChangedCallback(evt =>
+        {
+            ActiveItem.ItemID = evt.newValue;
+        });
+
+        ItemDetailsSection.Q<TextField>("ItemName").value = ActiveItem.ItemName;
+        ItemDetailsSection.Q<TextField>("ItemName").RegisterValueChangedCallback(evt =>
+        {
+            ActiveItem.ItemName = evt.newValue;
+            ItemListView.Rebuild();
+        });
+
+        IconPreview.style.backgroundImage = ActiveItem.ItemIcon == null ? DefaultIcon.texture : ActiveItem.ItemIcon.texture;
+        ItemDetailsSection.Q<ObjectField>("ItemIcon").RegisterValueChangedCallback(evt =>
+        {
+            Sprite newIcon = evt.newValue as Sprite;
+            ActiveItem.ItemIcon = newIcon;
+
+            IconPreview.style.backgroundImage = newIcon == null ? DefaultIcon.texture : newIcon.texture;
+            ItemListView.Rebuild();
+        });
+
+        ItemDetailsSection.Q<ObjectField>("ItemSprite").value = ActiveItem.ItemOnWorldSprite;
+        ItemDetailsSection.Q<ObjectField>("ItemSprite").RegisterValueChangedCallback(evt =>
+        {
+            ActiveItem.ItemOnWorldSprite = (Sprite)evt.newValue;
+        });
+
+        ItemDetailsSection.Q<EnumField>("ItemType").Init(ActiveItem.ItemType); //??
+        ItemDetailsSection.Q<EnumField>("ItemType").value = ActiveItem.ItemType;
+        ItemDetailsSection.Q<EnumField>("ItemType").RegisterValueChangedCallback(evt =>
+        {
+            ActiveItem.ItemType = (ItemType)evt.newValue;
+        });
+
+        ItemDetailsSection.Q<TextField>("Description").value = ActiveItem.ItemDescription;
+        ItemDetailsSection.Q<TextField>("Description").RegisterValueChangedCallback(evt =>
+        {
+            ActiveItem.ItemDescription = evt.newValue;
+        });
+
+        ItemDetailsSection.Q<IntegerField>("ItemUseRadius").value = ActiveItem.ItemUseRadius;
+        ItemDetailsSection.Q<IntegerField>("ItemUseRadius").RegisterValueChangedCallback(evt =>
+        {
+            ActiveItem.ItemUseRadius = evt.newValue;
+        });
+
+        ItemDetailsSection.Q<Toggle>("CanPickedup").value = ActiveItem.CanPicked;
+        ItemDetailsSection.Q<Toggle>("CanPickedup").RegisterValueChangedCallback(evt =>
+        {
+            ActiveItem.CanPicked = evt.newValue;
+        });
+
+        ItemDetailsSection.Q<Toggle>("CanDropped").value = ActiveItem.CanDropped;
+        ItemDetailsSection.Q<Toggle>("CanDropped").RegisterValueChangedCallback(evt =>
+        {
+            ActiveItem.CanDropped = evt.newValue;
+        });
+
+        ItemDetailsSection.Q<Toggle>("CanCarried").value = ActiveItem.CanCarried;
+        ItemDetailsSection.Q<Toggle>("CanCarried").RegisterValueChangedCallback(evt =>
+        {
+            ActiveItem.CanCarried = evt.newValue;
+        });
+
+        ItemDetailsSection.Q<IntegerField>("Price").value = ActiveItem.ItemPrice;
+        ItemDetailsSection.Q<IntegerField>("Price").RegisterValueChangedCallback(evt =>
+        {
+            ActiveItem.ItemPrice = evt.newValue;
+        });
+
+        ItemDetailsSection.Q<Slider>("SellPercentage").value = ActiveItem.SellPercentage;
+        ItemDetailsSection.Q<Slider>("SellPercentage").RegisterValueChangedCallback(evt =>
+        {
+            ActiveItem.SellPercentage = evt.newValue;
+        });
     }
 }
